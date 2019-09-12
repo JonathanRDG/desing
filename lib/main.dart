a883f6c9206ea2de92ce2098e7d41100abdc7216
jonathan <jr111_@hotmail.com>
2019-09-12 18:24:58 -0500

    finished page 1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basic',
+      initialRoute: 'scroll',
       routes:{
         'basic' : (BuildContext context) => BasicPage(),
         'scroll' : (BuildContext context)=> ScrollPage()
--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -4,8 +4,66 @@ class ScrollPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('scrollpage'),
+        child: Scaffold(
+          body: PageView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              _page1(),
+              _page2()
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  Widget _page1() {
+    return Stack(
+      children: <Widget>[
+        _backgroundColor(),
+        _backgroundImage(),
+        _text()
+      ],
+    );
+
+  }
+
+  Widget _page2() {}
+
+  Widget _backgroundColor() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108,192,218,1.0),
+
+    );
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+
+    );
+  }
+
+  Widget _text() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0,),
+          Text('11º', style: styleText),
+          Text('Miercoles', style: styleText),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
+    );
+  }
+
+  final styleText = TextStyle(color: Colors.white, fontSize: 50.0);
 }
